886b1925c89ef14181ae14e816a56fbc1bc114e8
DavidRaz19 <arazafimamonjydavid@gmail.com>
2025-05-03 15:42:39 +0300

    Create Administrateur.java

---
+++ com/hotel/model/Administrateur.java
@@ -0,0 +1,63 @@
+package com.hotel.model;
+
+public class Administrateur {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String username;
+    private String password;
+    
+    public Administrateur() {}
+    
+    public Administrateur(int id, String nom, String prenom, String username, String password) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.username = username;
+        this.password = password;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public String getPrenom() {
+        return prenom;
+    }
+    
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getNomComplet() {
+        return prenom + " " + nom;
+    }
+}
